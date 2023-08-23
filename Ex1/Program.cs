1eddc56006d96751f9e56dcc3546ca771b1f8590
JustCheka <cheka201@yandxe.ru>
2023-08-23 15:01:36 +0300

    Ex 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень ");
+int st = int.Parse(Console.ReadLine());
+
+int result = (int) Math.Pow(num, st);
+Console.Write(result);
