e1f29cbd65d8f0c8b0f9708803c22a46ba313965
JustCheka <cheka201@yandxe.ru>
2023-08-23 15:12:42 +0300

    Ex 3

---
+++ Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿void SetArray(int[] arr){
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++){
+        arr[i] = rand.Next(0, 1000);
+    }   
+}
+
+void PrintArray(int[] arr){
+    for (int i = 0; i < arr.Length; i++){
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = new int[8];
+
+
+SetArray(arr);
+PrintArray(arr);
